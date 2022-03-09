163eb1288c849344057d9a755fa2008b72ab92ab
stephenice <stephencode@hotmail.com>
2022-03-09 22:49:29 +0100

    Clear x Snippet refactoring

--- Clear_Button/script.js
+++ Clear_Button/script.js
@@ -3,19 +3,16 @@ const clearX = document.querySelector(".clearX");
 
 input_text.addEventListener("input", function (e) {
   console.log(e.target.value);
-  if (e.target.value && input_text.value) {
-    console.log("not empty");
-    //add x
-    clearX.textContent = "x";
-  } else {
-    console.log("empty");
-    // remove x
-    clearX.textContent = " ";
-  }
+  if (e.target.value && input_text.value) return (clearX.textContent = "x");
+  clear_toggle();
 });
 
 clearX.addEventListener("click", function () {
   console.log(this);
   input_text.value = "";
-  clearX.textContent = " ";
+  clear_toggle();
 });
+
+function clear_toggle() {
+  clearX.textContent = " ";
+}
