c2d8763e947c7dcf7a9a5526cbe468cdc89626c5
stephenice <stephencode@hotmail.com>
2022-03-09 21:01:03 +0100

    Clear x Snippet

--- Clear_Button/script.js
+++ Clear_Button/script.js
@@ -1,12 +1,21 @@
 const input_text = document.querySelector("input");
-const clear = document.querySelector(".clear");
+const clearX = document.querySelector(".clearX");
 
 input_text.addEventListener("input", function (e) {
   console.log(e.target.value);
   if (e.target.value && input_text.value) {
     console.log("not empty");
-    console.log(input_text);
+    //add x
+    clearX.textContent = "x";
   } else {
     console.log("empty");
+    // remove x
+    clearX.textContent = " ";
   }
+});
+
+clearX.addEventListener("click", function () {
+  console.log(this);
+  input_text.value = "";
+  clearX.textContent = " ";
 });
