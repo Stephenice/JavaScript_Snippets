b0b31f9ba5235f4885169bc3e392d11a9c7876a6
stephenice <stephencode@hotmail.com>
2022-03-24 18:59:41 +0100

    deep clone with Lo_Dash

--- 4. clone Object with Lo_dash/script.js
+++ 4. clone Object with Lo_dash/script.js
@@ -1,1 +1,18 @@
 "use strict";
+//const _ = require("lodash");
+
+const ver = _.VERSION;
+console.log(ver);
+
+const person2 = {
+  first_name: "mary",
+  last_name: "william",
+  age: 21,
+  family: ["father", "mother", "sister"],
+};
+
+const married2 = _.cloneDeep(person2);
+married2.last_name = "bill";
+married2.family.push("father in_law");
+console.log("before", person2);
+console.log("after", married2);
