87fa3515e80a4405cf3fa0e96484f3ac9a49d69d
stephenice <stephencode@hotmail.com>
2022-03-24 16:34:48 +0100

    Copying object shallow copy update

--- 3. primitive Vs reference values/script.js
+++ 3. primitive Vs reference values/script.js
@@ -14,7 +14,23 @@ const person = {
   age: 21,
 };
 
+//this not copying object bcos its a reference
 const married = person;
 married.last_name = "bill";
 console.log("before", person);
 console.log("after", married);
+
+/****************** Copying object shallow copy *********/
+
+const person2 = {
+  first_name: "mary",
+  last_name: "william",
+  age: 21,
+  family: ["father", "mother", "sister"],
+};
+
+const married2 = Object.assign({}, person2);
+married2.last_name = "bill";
+married2.family.push("father in_law");
+console.log("before", person2);
+console.log("after", married2);
