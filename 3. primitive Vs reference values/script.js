3ec3fbc2e6665ff90481fe16f49c636ff28c42bf
stephenice <stephencode@hotmail.com>
2022-03-24 16:16:18 +0100

    Primitive update

--- 3. primitive Vs reference values/script.js
+++ 3. primitive Vs reference values/script.js
@@ -9,6 +9,12 @@ console.log(b);
 
 /****************** Reference *********/
 const person = {
-  first_name: "Jame",
+  first_name: "mary",
   last_name: "william",
+  age: 21,
 };
+
+const married = person;
+married.last_name = "bill";
+console.log("before", person);
+console.log("after", married);
