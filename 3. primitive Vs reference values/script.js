4718ceaa1ac6365e8ec166da629f3599b4898120
stephenice <stephencode@hotmail.com>
2022-03-24 15:23:26 +0100

    Primitive update

---
+++ 3. primitive Vs reference values/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/****************** Primitive *********/
+let a = 21;
+let b = a;
+a = 61;
+console.log(a);
+console.log(b);
+
+/****************** Reference *********/
+const person = {
+  first_name: "Jame",
+  last_name: "william",
+};
