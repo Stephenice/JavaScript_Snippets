8d147f3e9942b9a609489e10e3e072fdaaf1b93b
stephenice <stephencode@hotmail.com>
2022-03-25 19:17:37 +0100

    Destructuring Arrays (Switching varible)

--- 5. Destructuring Arrays/script.js
+++ 5. Destructuring Arrays/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const shoppingList = {
+  kitchenItems: ["cups", "cutlery", "bin", "dish dryer"],
+  food: ["rice", "bean", "bread"],
+};
+
+let [firstItem, secondItem, thirdItem] = shoppingList.food;
+console.log(firstItem, secondItem);
+
+// switching
+[firstItem, secondItem] = [secondItem, firstItem];
+console.log(firstItem, secondItem);
