9b333d6f8914d1fc889799e1713a3e9ac2e4efaf
stephenice <stephencode@hotmail.com>
2022-03-28 21:27:19 +0200

    Destructuring Arrays

--- 5. Destructuring Arrays/script.js
+++ 5. Destructuring Arrays/script.js
@@ -3,6 +3,10 @@
 const shoppingList = {
   kitchenItems: ["cups", "cutlery", "bin", "dish dryer"],
   food: ["rice", "bean", "bread"],
+
+  order: function (item1, item2) {
+    return [this.kitchenItems[item1], this.kitchenItems[item2]];
+  },
 };
 
 let [firstItem, secondItem, thirdItem] = shoppingList.food;
@@ -11,3 +15,7 @@ console.log(firstItem, secondItem);
 // Swapping variables
 [firstItem, secondItem] = [secondItem, firstItem];
 console.log(firstItem, secondItem);
+
+//function
+const [a, b] = shoppingList.order(0, 1);
+console.log(a, b);
