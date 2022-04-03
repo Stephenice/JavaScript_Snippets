458b62d65c7cd8da0299080d9dedb94cd0aebe56
stephenice <stephencode@hotmail.com>
2022-04-04 00:03:32 +0200

    Spread Opertor

--- 11. Spread Operator/script.js
+++ 11. Spread Operator/script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const arr = [1, 5, 7, 3, 4];
+const arr2 = [99, 33, 55, 22, 11];
+const numbers = [...arr];
+
+//seperated by space
+console.log(...numbers);
+
+//shallow copy
+numbers.push(2);
+console.log(numbers);
+console.log(arr);
+
+//join 2 array
+const arrNew = [...arr, ...arr2];
+console.log(arrNew);
+console.log(...arrNew);
+
+const name = "stephen";
+console.log([...name]);
+
+//function
+function food(f1) {
+  console.log(...f1);
+}
+
+food(["rice", "bean", "bread"]);
