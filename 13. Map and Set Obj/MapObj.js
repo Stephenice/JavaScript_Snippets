e96a2f8650e8cd76fcb9170a56eb58226903be38
stephenice <stephencode@hotmail.com>
2022-05-12 15:11:19 +0200

    Update Map Object

--- 13. Map and Set Obj/MapObj.js
+++ 13. Map and Set Obj/MapObj.js
@@ -16,3 +16,25 @@ console.log(getName);
 //To check if a Map has a key use: has(key name)-
 const checkAge = myMapObj.has("age");
 console.log(checkAge);
+
+// To delete a property in Map use: delete(key name)
+// console.log(myMapObj.delete("age"));
+console.log(myMapObj);
+
+// To check the size or length of Map use: size
+console.log(myMapObj.size);
+
+// to clear the Map Object use: clear()
+// console.log(myMapObj.clear()); //return undefined
+console.log(myMapObj); //return 0
+
+// How to use Array and Object as key and called the array value
+const friend = ["james", "mary", "william"];
+myMapObj.set(friend, true);
+const result = myMapObj.get(friend);
+console.log(myMapObj);
+
+// How to create a Map and set multiple key and value.
+// Convert Object to Map use: Object.entries(object name)
+// Looping Map
+// Convert Map to array use: [...map name]
