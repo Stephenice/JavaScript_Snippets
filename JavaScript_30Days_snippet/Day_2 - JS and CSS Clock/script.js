4daf35364fb90faf733bb8aa7a34a4ef1c40647f
stephenice <stephencode@hotmail.com>
2022-05-23 15:45:56 +0200

    Day 2 JavaScript: clock Completed

--- JavaScript_30Days_snippet/Day_2 - JS and CSS Clock/script.js
+++ JavaScript_30Days_snippet/Day_2 - JS and CSS Clock/script.js
@@ -1,12 +1,25 @@
 "use strict";
 const secondHand = document.querySelector(".second-hand");
+const minsHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
   const now = new Date();
+
+  // second hand
   const seconds = now.getSeconds();
   const secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  console.log(seconds);
+
+  // mins
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  // hours
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 60) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
