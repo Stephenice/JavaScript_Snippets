18143fbfc10caa79daa18a4dddba1fb845f6a97b
stephenice <stephencode@hotmail.com>
2022-05-23 02:32:54 +0200

    Min and Max Completed

---
+++ interview_Question/2.min_max.js
@@ -0,0 +1,17 @@
+const min_max = [3, 8, -2, 0, 10];
+
+function minMax(min_max) {
+  const element = min_max;
+  let min = (max = element[0]);
+
+  for (let index = 0; index < element.length; index++) {
+    if (element[index] < min) {
+      min = element[index];
+    } else if (element[index] > max) {
+      max = element[index];
+    }
+  }
+  console.log(min, max);
+}
+
+minMax(min_max);
