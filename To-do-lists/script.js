914b05edbf6458064fe4fab057d066417d0064c6
stephenice <stephencode@hotmail.com>
2022-07-24 00:15:43 +0100

    snippet todo

--- To-do-lists/script.js
+++ To-do-lists/script.js
@@ -1,17 +1,41 @@
 const text = document.querySelector(".text");
 const date = document.querySelector(`[type="date"]`);
 const submit = document.querySelector("button");
+const ui_lists = document.querySelector(".ui-lists");
 
 const toDoListArray = [];
-const toDoListObject = {
-  text,
-  date,
+
+const dataResult = (e) => {
+  const toDoListObject = {
+    text,
+    date,
+  };
+  //get the value of the to-do input
+  toDoListObject.text = text.value;
+  toDoListObject.date = date.value;
+
+  // add value to the state
+  toDoListArray.push(toDoListObject);
+
+  // render data to the UI
+  render(toDoListArray);
 };
 
-submit.addEventListener("click", () => {
-  toDoListObject.text = "today";
-  toDoListObject.date = date.value;
-  console.log(toDoListObject);
-  toDoListArray.push(toDoListObject);
-  console.log(toDoListArray);
-});
+const render = (data) => {
+  const indexData = data.length - 1;
+
+  const html = `
+   <li> ${data[indexData].text} ${data[indexData].date} 
+   <button class="delete">Delete</button> </li>
+   `;
+  const del = document.querySelectorAll(".delete");
+  console.log(del);
+
+  if (del.length > 0) {
+    console.log("yes");
+  }
+
+  ui_lists.insertAdjacentHTML("beforeend", html);
+};
+
+submit.addEventListener("click", dataResult);
